7b253a7eef30f43744cfd657df25fda90ebb4614
yahk <ygall@ahorra.io>
2025-01-05 21:13:30 -0600

    se agregan cambios listos para el despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel()
 });
